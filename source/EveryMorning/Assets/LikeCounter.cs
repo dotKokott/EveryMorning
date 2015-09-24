1f58dcc9dce1fc0e7f04327eccfb7e16d5794b81
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-09-25 00:10:31 +0200

    likecounter

---
+++ source/EveryMorning/Assets/LikeCounter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LikeCounter : MonoBehaviour {
+
+    public static LikeCounter _ {
+        get { return instance; }
+    }
+    private static LikeCounter instance;
+
+    private Text label;
+
+    // Use this for initialization
+    void Start() {
+        label = GetComponent<Text>();
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( Input.GetKeyDown( KeyCode.Space ) ) {
+            StartCount( Random.Range( 0, 100 ) );
+        }
+    }
+
+    public void StartCount( int max ) {
+        iTween.ValueTo( gameObject, iTween.Hash(
+            "time", 3, "from", 0, "to", max,
+            "onupdate", "updatecounter",
+            "easetype", iTween.EaseType.easeOutBack ) );
+    }
+
+    private void updatecounter( float value ) {
+        label.text = string.Format( "{0} likes", Mathf.FloorToInt( value ) );
+    }
+}
