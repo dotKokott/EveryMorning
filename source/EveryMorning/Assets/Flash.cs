8d7e5e5277e7de80756aebd22e78e54c2ca162b8
Christian Kokott <christian.kokott@gmail.com>
2015-09-24 23:32:22 +0200

    batteries

--- source/EveryMorning/Assets/Flash.cs
+++ source/EveryMorning/Assets/Flash.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Flash : MonoBehaviour {
 
+    public Color color;
+
 	// Use this for initialization
 	void Start () {
-        
+        iTween.ColorTo(gameObject, iTween.Hash("color", color, "time", 0.5f, "looptype", iTween.LoopType.pingPong));
 	}
 	
 	// Update is called once per frame
